a0784944a34ff455486364800a121008bae36921
David <damadi.dev@gmail.com>
2015-06-07 18:56:39 +0000

    Cambios en el proyecto - Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var quizcontroller = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('quizes/answer',quizcontroller.answer);
+router.get('quizes/question',quizcontroller.question);
 
 module.exports = router;
