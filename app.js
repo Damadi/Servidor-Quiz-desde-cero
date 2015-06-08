5aa03ee1d30fc4c8be4e7c9cd4f77112f9ea2f9d
David <damdi.dev@gmail.com>
2015-06-08 18:46:40 +0100

    Quiz 4: Marco de la aplicación

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials')
 
 var routes = require('./routes/index');
 
 var app = express();
 
+app.use(partials());
 app.use('/', routes);
 
 // view engine setup
@@ -24,7 +26,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
