9b84361108f86df4d41caa4f47c6f7357a4d5fca
David <damdi.dev@gmail.com>
2015-07-23 11:14:58 +0100

    cambio para Heroku

--- models/models.js
+++ models/models.js
@@ -5,7 +5,7 @@ var path = require('path');
 var DATABASE_URL="sqlite://:@:/";
 var DATABASE_STORAGE="quiz.sqlite";
 
-var url = DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name 	= (url[6]||null);
 var user 		= (url[2]||null);
 var pwd 		= (url[3]||null);
@@ -13,7 +13,7 @@ var protocol 	= (url[1]||null);
 var dialect 	= (url[1]||null);
 var port 		= (url[5]||null);
 var host	    = (url[4]||null);
-var storage 	= DATABASE_STORAGE;
+var storage 	= process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -28,25 +28,26 @@ var sequelize = new Sequelize(DB_name,user,pwd,{
 	omitNull: true // solo Postgres
 });
 
-//Importar la definición de la tabla Quiz en quiz.js
+// Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-//Importar la definición de la tabla Comment
-
+// Importar definicion de la tabla Comment
 var comment_path = path.join(__dirname,'comment');
 var Comment = sequelize.import(comment_path);
 
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
-exports.Quiz = Quiz;
-exports.Comment = Comment;
+// exportar tablas
+exports.Quiz = Quiz; 
+exports.Comment = Comment; 
 
 // sequelize.sync() crea e inicializa tabla preguntas en DB
 sequelize.sync().success(function(){
 	//success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	console.log('Inicializada base de datos');
+	Quiz.count().success(function(count){		
 		if(count===0){
 			Quiz.create({
 				pregunta: 'Capital de Italia',
