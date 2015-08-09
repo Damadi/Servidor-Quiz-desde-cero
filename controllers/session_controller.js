d52cd6eab6371009cb63a3a9d8bba2f4bf070ded
David <damdi.dev@gmail.com>
2015-08-09 19:38:58 +0100

    M9 Tema 3:  Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,13 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+exports.loginRequired = function(req, res, next){
+    if(req.session.user){
+        next();    
+    }else{
+        res.redirect('/login');
+    }
+};
+
 // Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
